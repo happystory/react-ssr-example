951b291a826818726b7c97f557eb01286df26919
liuyao <670652018@qq.com>
2018-10-10 23:07:16 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import Home from './containers/Home';
+
+const app = express();
+const content = renderToString(<Home />);
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>ssr</title>
+      </head>
+      <body>
+        ${content}
+      </body>
+    </html>
+  `);
+});
+
+app.listen(3000);
---
+++ webpack.server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'development',
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  }
+};
