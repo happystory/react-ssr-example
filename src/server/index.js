feb912e16db1c552e73dc0bdfed65ed97febddeb
liuyao <670652018@qq.com>
2018-10-11 23:27:17 +0800

    router

--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,24 @@
 import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import Home from '../containers/Home';
+import { StaticRouter } from 'react-router-dom';
+import Routes from '../Routes';
 
 const app = express();
 app.use(express.static('public'));
 
-const content = renderToString(<Home />);
 const livereloadTpl = `<script>
 document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
 ':35728/livereload.js?snipver=1"></' + 'script>')
 </script>`;
 
 app.get('/', (req, res) => {
+  const content = renderToString((
+    <StaticRouter location={req.path} context={{}}>
+      { Routes }
+    </StaticRouter>
+  ));
+
   res.send(`
     <html>
       <head>
